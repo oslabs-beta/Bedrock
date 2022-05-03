ddb796a1b9c748e983c7e07f5c8ef8cd61412c0f
Julian Kang <julianswkang@gmail.com>
2022-05-03 14:19:26 -0400

    added comments of details within localstrategy.ts

--- src/LocalStrategy.ts
+++ src/LocalStrategy.ts
@@ -23,7 +23,10 @@ export type LocalStrategyParams = {
   mfa_enabled : false;
   readCreds? : (ctx: Context) => string[];
 }
-
+/**
+ * Class LocalStrategy has 2 REQUIRED properties: checkCreds and mfa_enabled
+ * All other properties are optional, and will be subjected to type LocalStrategyParams
+ */
 export class LocalStrategy {
   checkCreds: ((username: string, password: string) => Promise<boolean>);
   mfa_enabled: boolean; 
@@ -40,7 +43,19 @@ export class LocalStrategy {
 
     Object.assign(this, stratParams);
   }
-  
+  /**
+   * @param context object
+   * @param next function
+   * The localLogin method leverages the checkCreds property that was initialized when the object was instantiated 
+   * It may also utilize the optional readCreds property that the developer may use in order to provide an array of [username, password]
+   * If readCreds is not initialized, will assume the developer is utilizing the Authorization header Basic to pass username and password in base64
+   
+   * Will leverage the checkCreds property with the passed in username and password. 
+      If returns true, will set session 'isLoggedIn' to true and initialize state property 'localVerified' to true.
+      Else, will set 'isLoggedIn' to false and initialize state property 'localVerified' to false.
+
+   * Developer may then leverage the localVerified property on state in subsequent middleware 
+   */
   localLogin = async (ctx: Context, next: () => Promise<unknown>) => {
     let credentials: string[] = [];
 
@@ -76,7 +91,16 @@ export class LocalStrategy {
     // and send response based off auth status
   }
   /**
-   * 
+   * @param context object
+   * @param next function
+   * Note: verifyAuth is an OPTIONAL authorization verifying middleware function for the developer to utilize
+       This may be deferred, as developer may utilize different means of verifying authorization prior to allowing client access to sensitive material
+   * verifyAuth checks the isLoggedIn and mfa_success session properties previously set by checkMFA
+   * Will ensure that isLoggedIn is true
+      Then will check to see if mfa_enabled is true -- if so, will ensure previous mfaCheck set property mfa_success to true
+        If mfa_success is true, will then allow progression
+   *  Otherwise, if mfa_enabled is false, will also allow progression since mfa_success check is not warranted
+   * If isLoggedIn is false, will return message stating client is "Not currently signed in"
    */
   verifyAuth = async (ctx: Context, next: () => Promise<unknown>) => {
     console.log('route hit - details below');
@@ -99,7 +123,15 @@ export class LocalStrategy {
     return;
   }
   /**
-   * @params: 
+   * @param context object
+   * @param next function
+   * Invoking checkMFA will utilize the object's getSecret property (a function defined by the developer)
+   * This will utilize the Oak session to obtain the client username in order to utilize getSecret
+   * Upon obtaining the username's associated secret, will invoke the imported generateTOTP() function
+   * Checks to see if the input code from client matches the generated TOTP
+   *  Note: the developer will need to ensure that the client's MFA input is passed into the 
+      context.request body as the property, 'code'
+   * If verified, will initialize session 'mfa_success' as set to true. Else, will initialize to false
    */
   checkMFA = async (ctx: Context, next: () => Promise<unknown>) => {
     const body = await ctx.request.body();
@@ -127,10 +159,12 @@ export class LocalStrategy {
     }
   }
   /**
-   * @params: context object
+   * @param context object
    * Invoking sendMFA will send a 6 digit code via one of two methods: SMS or e-mail
+   * Will check to see which mfa_type is initialized
    * If mfa_type is SMS, will instantiate an object from the TwilioSMS class with the accountSID and authtoken provided from Twilio
-   *  Will then invoke the TwilioSMS method's sendSMS function while passing in an object that designates the 'From' phone number (developer's designated Twilio phone number) and 'To' phone number (client/user's phone number)
+   *  Will then invoke the TwilioSMS method's sendSMS function while passing in an object that designates 
+      the 'From' phone number (developer's designated Twilio phone number) and 'To' phone number (client/user's phone number)
    */
   sendMFA = async (ctx: Context) => {
     if (this.mfa_type === "SMS") {
@@ -143,7 +177,12 @@ export class LocalStrategy {
     }
     //Insert ELSE IF statement for when email is set up
   }
-
+  /**
+   * 
+   * @param context object
+   * @param next function
+   * When invoked, will delete the current session from the client 
+   */
   signOut = async (ctx: Context, next: () => Promise<unknown>) => {
     await ctx.state.session.deleteSession(ctx);
     next();
